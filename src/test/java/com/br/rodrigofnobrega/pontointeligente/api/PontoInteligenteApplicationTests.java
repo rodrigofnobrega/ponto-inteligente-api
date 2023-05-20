dab6f3fdff553c89d63f7b897f70b5f7dec3faf7
rodrigofnobrega <rofen1@protonmail.com>
2023-05-20 20:57:44 -0300

    Commit inicial dos arquivos do projeto

---
+++ src/test/java/com/br/rodrigofnobrega/pontointeligente/api/PontoInteligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.rodrigofnobrega.pontointeligente.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoInteligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
