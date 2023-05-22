b1cc6e02cd2232fa51d010efa33c63fd072873cc
rodrigofnobrega <rofen1@protonmail.com>
2023-05-21 23:52:36 -0300

    Criação da estrutura do MySQL

---
+++ src/main/java/com/br/rodrigofnobrega/pontointeligente/api/enums/PerfilEnum.java
@@ -0,0 +1,5 @@
+package com.br.rodrigofnobrega.pontointeligente.api.enums;
+
+public enum PerfilEnum {
+	ROLE_ADMIN, ROLE_USUARIO;
+}
---
+++ src/main/java/com/br/rodrigofnobrega/pontointeligente/api/enums/TipoEnum.java
@@ -0,0 +1,7 @@
+package com.br.rodrigofnobrega.pontointeligente.api.enums;
+
+public enum TipoEnum {
+	INICIO_TRABALHO, TERMINO_TRABALHO, 
+	INICIO_ALMOCO, TERMINO_ALMOCO,
+	INICIO_PAUSA, TERMINO_PAUSA;
+}
